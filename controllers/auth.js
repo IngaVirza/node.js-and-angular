85d74031fa277fbe445e9cb16ffd00d21fded450
unknown <inha.virza@gmail.com>
2024-08-28 19:23:59 +0200

    Error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('./models/User');
 const keys = require('../config/keys');
+const errorHandler = require('./utils/errorHandler');
 
 module.exports.login = async function (req, res) {
   const candidate = await User.findOne({ email: req.body.email });
@@ -62,7 +63,7 @@ module.exports.register = async function (req, res) {
       await user.save();
       res.status(201).json(user);
     } catch (e) {
-      // Handle the error
+      errorHandler(res, e);
     }
   }
 };
