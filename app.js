1506e9f963c56bf48f077f09ecb93893ab57e681
unknown <inha.virza@gmail.com>
2024-08-31 02:11:33 +0200

    Creatung categories

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ mongoose
 app.use(passport.initialize());
 require('./meddleware/passport')(passport);
 app.use(require('morgan')('dev'));
+app.use('uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require('cors')());
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 const keys = require('../config/keys');
-const errorHandler = require('./utils/errorHandler');
+const errorHandler = require('../utils/errorHandler');
 
 module.exports.login = async function (req, res) {
   const candidate = await User.findOne({ email: req.body.email });
