51c16393f28a36c400d9cbf91a5f86681c1986e4
unknown <inha.virza@gmail.com>
2024-08-31 18:37:13 +0200

    routings

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  NgModule,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter, RouterModule, Routes } from '@angular/router';
 
-import { routes } from './app.routes';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+const routes: Routes = [{ path: 'login', component: LoginPageComponent }];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
