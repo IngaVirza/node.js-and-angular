d72b5a524656e73f6312e779fe2d54fa7201e186
unknown <inha.virza@gmail.com>
2024-08-31 14:17:52 +0200

    Order functionality

--- routes/position.js
+++ routes/position.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const controller = require('../controllers/position');
+const passport = require('passport');
 const router = express.Router();
 
-router.post('/:categoryId', controller.getByCategoryId);
-router.post('/', controller.create);
-router.post('/:id', controller.update);
-router.post('/:id', controller.remove);
+router.get(
+  '/:categoryId',
+  passport.authenticate('jwt', { session: false }),
+  controller.getByCategoryId
+);
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  controller.create
+);
+router.patch(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  controller.update
+);
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  controller.remove
+);
 
 module.exports = router;
